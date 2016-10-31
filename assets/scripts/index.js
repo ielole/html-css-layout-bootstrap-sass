294bc83e21e98463aeed0f249f6acdb1dc85a658
ielole <ieloletin@gmail.com>
2016-10-31 17:29:53 -0400

    Code-along main view for game with bootstrap.
    Coded along to create:
    - navigation bar
    - modal for:
      - sign(up/in/out)
      - change password
    - div board

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,19 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+//on document ready
+$(()=>{
+$('.sign-up-form').on('submit', function(e){
+  e.preventDefault();
+  let $coolBro = $('#sign-up-email').val();
+  //$ referring to jQuery doesn't have an actual fxn beside as a sign post
+  //debugger;
+
+  console.log(coolBro);
+
+  $('#sign-up-modal').modal('hide');
+  //use above to hide stuff instead of javascript .hide
+  //think about bubbling up especially when only being able to click a square once
+  });
+});
